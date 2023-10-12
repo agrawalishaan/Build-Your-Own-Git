3e2cb40ff84459c10d9c8c710107b373b2a6adab
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-10-11 23:25:34 -0400

    add basic read blob function

--- app/main.js
+++ app/main.js
@@ -1,28 +1,48 @@
 const fs = require("fs");
 const path = require("path");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+// sources from terminal
+const gitCommand = process.argv[2];
 
-// Uncomment this block to pass the first stage
-const command = process.argv[2];
-
-switch (command) {
-  case "init":
-    createGitDirectory();
-    break;
-  default:
-    throw new Error(`Unknown command ${command}`);
-}
-
-function createGitDirectory() {
+// create .git folder and its subfolders. Defensively set { recursive: true } in case we refactor
+function gitInit() {
   fs.mkdirSync(path.join(__dirname, ".git"), { recursive: true });
   fs.mkdirSync(path.join(__dirname, ".git", "objects"), { recursive: true });
   fs.mkdirSync(path.join(__dirname, ".git", "refs"), { recursive: true });
-
+  // create the HEAD, overwrites if a HEAD exists
   fs.writeFileSync(
     path.join(__dirname, ".git", "HEAD"),
     "ref: refs/heads/master\n"
   );
-  console.log("Initialized git directory");
+  console.log("git repository initialized");
+}
+
+function printBlob(blobSHA) {
+  const blobPath = path.join(
+    __dirname,
+    ".git",
+    "objects",
+    blobSHA.slice(0, 2),
+    blobSHA.slice(2)
+  );
+  const blob = fs.readFileSync(blobPath);
+  console.log(blob);
+}
+
+switch (gitCommand) {
+  case "init":
+    gitInit();
+    break;
+  case "cat-file":
+    const flag = process.argv[3];
+    const blobSHA = process.argv[4];
+    if (flag === "-p") {
+      // pretty print
+      printBlob(blobSHA);
+    } else {
+      throw new Error(`Unknown flag ${flag}`);
+    }
+    break;
+  default:
+    throw new Error(`Unknown command ${command}`);
 }
